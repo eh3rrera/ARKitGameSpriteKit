b1ed3a25d6dcd8b0098d580fd309d46b96dd0d54
Esteban Herrera <eh@MacBook-Pro-de-Esteban.local>
2017-07-09 20:06:28 -0500

    Disable plane tracking

--- ARSpriteKit/ViewController.swift
+++ ARSpriteKit/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingSessionConfiguration()
-        configuration.planeDetection = .horizontal
+        //configuration.planeDetection = .horizontal
         
         // Run the view's session
         sceneView.session.run(configuration)
